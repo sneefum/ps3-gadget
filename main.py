e5e11dca32b1dd95293bb85504d3437b1f72d5d5
sneefum <216181137+sneefum@users.noreply.github.com>
2026-07-28 18:58:43 +0100

    Fixed bugs and added testing

--- main.py
+++ main.py
@@ -2,8 +2,34 @@ import ps3
 
 con = ps3.Controller()
 
-print(con.left_stick)
+con.left_stick.tilt_stick(1, 3) 
+# x: 1, y: 3. normalizes to length 1
+# (1, 3) -> (1/root 10, 3/root 10) ~= (0.316, 0.949)
 
-con.left_stick.tilt_stick(1, 0)
+con.right_stick.tilt_stick(0.1, 0)
+# x: 0.1, y: 0. length < 1, no need to normalize.
 
-print(con.left_stick)
+con.buttons.press_button(ps3.Button.L2)
+con.buttons.press_button(ps3.Button.R2) # Press the square button (obviously)
+con.buttons.press_button(ps3.Button.L1) # PlayStation button, has special behavior
+con.buttons.press_button(ps3.Button.Circle)
+con.buttons.press_button(ps3.Button.X)
+con.buttons.press_button(ps3.Button.Select)
+con.buttons.press_button(ps3.Button.L3)
+con.buttons.press_button(ps3.Button.R3)
+con.buttons.press_button(ps3.Button.Down)
+con.buttons.press_button(ps3.Button.Right)
+
+
+packet = con.generate_input_packet()
+
+packet_readable = ""
+
+count = 0
+for byte in packet:
+    if count % 7 == 0 and count != 0:
+        packet_readable += "\n"
+    packet_readable += f"{byte:02X} "
+    count += 1
+
+print(packet_readable) # Print the bytes that get sent over USB as an input
--- ps3.py
+++ ps3.py
@@ -1,10 +1,12 @@
 from typing import Iterator
 import os
+import math
+from enum import Enum
 
 if os.geteuid() != 0:
     raise PermissionError("This module must be ran as root!")
 
-class Button:
+class Button(Enum):
     L2 =       0x0001
     R2 =       0x0002
     L1 =       0x0004
@@ -22,7 +24,44 @@ class Button:
     Down =     0x4000
     Left =     0x8000
 
-    X = Cross # Some people call it X instead of Cross, this is here to prevent naming issues
+    PS =       0x10000 # Special Case
+
+    X =         Cross # Some people call it X instead of Cross, this is here to prevent naming issues
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return f"Button({self.name})"
+
+class Buttons:
+    def __init__(self) -> None:
+        self.pressed = 0x0000
+        self.ps_button = False
+
+    def __bytes__(self) -> bytes:
+        return self.pressed.to_bytes(2, "big")
+
+    def __iter__(self) -> Iterator[Button]:
+        return iter([Button(1 << i) for i in range(16) if self.pressed & (1 << i) > 0])
+
+    def __repr__(self) -> str:
+        return f"Buttons({str(self.buttons_pressed())[1:-1]})"
+
+    def buttons_pressed(self) -> list[str]:
+        return [str(Button(1 << i)) for i in range(16) if self.pressed & (1 << i) > 0]
+
+    def press_button(self, button: Button) -> None:
+        if button == Button.PS:
+            self.ps_button = True
+        else:
+            self.pressed |= button.value
+
+    def release_button(self, button: Button) -> None:
+        if button == Button.PS:
+            self.ps_button = False
+        else:
+            self.pressed &= ~button.value & 0xFFFF
 
 class Stick:
     def __init__(self, x=0, y=0) -> None:
@@ -30,42 +69,46 @@ class Stick:
         self.y = y
 
     def __bytes__(self) -> bytes:
-        return bytes([round(self.x + 1 * 127.5), round(self.y + 1 * 127.5)])
+        return bytes([round((self.x + 1) * 127.5), round((self.y + 1) * 127.5)])
 
     def __iter__(self) -> Iterator[int]:
-        return iter([round(self.x + 1 * 127.5), round(self.y + 1 * 127.5)])
+        return iter([round((self.x + 1) * 127.5), round((self.y + 1) * 127.5)])
 
     def __repr__(self) -> str:
         return f"Stick({self.x}, {self.y})"
 
-    def tilt_stick(self, x, y) -> None:
-        self.x = x
-        self.y = y
+    def tilt_stick(self, x: float, y: float) -> None:
+        magnitude = max(1, ((x*x) + (y*y))**0.5)
+        self.x = x / magnitude
+        self.y = y / magnitude
+
+    def tilt_stick_angle(self, angle: float, magnitude: float) -> None:
+        self.tilt_stick(math.sin(angle) * magnitude, math.cos(angle) * magnitude)
 
 class Controller:
-    def __init__(self) -> None:
-        self.buttons = []
-        self.ps = False
+    def __init__(self, dev: str = "/dev/hidg0") -> None:
+        self.dev = dev
+
+        self.buttons = Buttons()
         
         self.left_stick = Stick()
         self.right_stick = Stick()
 
         # TODO: Implement Accelerometer and Gyroscope
-        # TODO: Proper handling of button pressure and analog L2 and R2
     
-    def send_input(self, dev="/dev/hidg0") -> None:
-        with open(dev, "wb") as hid:
+    def send_input(self) -> None:
+        with open(self.dev, "wb") as hid:
             packet = self.generate_input_packet()
             hid.write(packet)
 
-    def generate_input_packet(self) -> bytearray:
+    def generate_input_packet(self) -> bytes:
         packet = [] # 49 byte packet
 
         packet.append(0x01) # Report ID (1 => Input Packet)
         packet.append(0x00) # Unknown
-        packet.append(self.buttons >> 8 % 255) # Buttons
-        packet.append(self.buttons % 255) # Buttons
-        packet.append(int(self.ps)) # Playstation Button
+        packet.append(bytes(self.buttons)[0]) # Buttons High Byte
+        packet.append(bytes(self.buttons)[1]) # Buttons Low Byte
+        packet.append(int(self.buttons.ps_button)) # Playstation Button
         packet.append(0x00) # Unknown
         packet.extend(list(self.left_stick)) # Left Stick
         packet.extend(list(self.right_stick)) # Right Stick
@@ -73,18 +116,18 @@ class Controller:
         packet.append(0x00) # TODO: Something called move_power_status
         packet.append(0x00) # Unknown
         packet.extend([
-            255 * self.buttons & Button.Up > 0,
-            255 * self.buttons & Button.Right > 0,
-            255 * self.buttons & Button.Down> 0,
-            255 * self.buttons & Button.Left > 0,
-            255 * self.buttons & Button.L2 > 0,
-            255 * self.buttons & Button.R2 > 0,
-            255 * self.buttons & Button.L1 > 0,
-            255 * self.buttons & Button.R1 > 0,
-            255 * self.buttons & Button.Triangle > 0,
-            255 * self.buttons & Button.Circle > 0,
-            255 * self.buttons & Button.Cross > 0,
-            255 * self.buttons & Button.Square> 0
+            255 * (self.buttons.pressed & Button.Up.value > 0),
+            255 * (self.buttons.pressed & Button.Right.value > 0),
+            255 * (self.buttons.pressed & Button.Down.value > 0),
+            255 * (self.buttons.pressed & Button.Left.value > 0),
+            255 * (self.buttons.pressed & Button.L2.value > 0),
+            255 * (self.buttons.pressed & Button.R2.value > 0),
+            255 * (self.buttons.pressed & Button.L1.value > 0),
+            255 * (self.buttons.pressed & Button.R1.value > 0),
+            255 * (self.buttons.pressed & Button.Triangle.value > 0),
+            255 * (self.buttons.pressed & Button.Circle.value > 0),
+            255 * (self.buttons.pressed & Button.Cross.value > 0),
+            255 * (self.buttons.pressed & Button.Square.value > 0)
         ]) # Order of these buttons is different from the actual pressed buttons, so I have to do it manually.
         packet.extend([0x00] * 3) # Unknown
         packet.extend([0x02, 0xEE, 0x10]) # Controller Status: Plugged In, Charging, Wired with Rumble Enabled
@@ -96,6 +139,29 @@ class Controller:
         if len(packet) < 49:
             raise ValueError(f"Packet is too short! Expected 49 bytes and got {len(packet)}!")
 
-        packet_bytes = bytearray(packet)
+        packet_bytes = bytes(packet)
 
-        return packet_bytes 
+        return packet_bytes
+
+#if __name__ == "__main__":
+#    print("Running self test!")
+#
+#    con = Controller()
+#
+#    print(con.buttons)
+#    print(con.buttons.pressed)
+#    con.buttons.press_button(Button.Square)
+#    print(con.buttons)
+#    print(con.buttons.pressed)
+#    print(Button.Square)
+#
+#    print(con.left_stick)
+#    print(con.right_stick)
+#    con.left_stick.tilt_stick(0, 0.1)
+#    con.right_stick.tilt_stick(100, 700)
+#    print(con.left_stick)
+#    print(con.right_stick)
+#    con.left_stick.tilt_stick(-1, -1)
+#    con.right_stick.tilt_stick(1, 1)
+#
+#    print(con.generate_input_packet())
